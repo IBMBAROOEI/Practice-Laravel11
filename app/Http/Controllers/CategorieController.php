a1d10b5ccb2599ac7730e8ea06ee0eaa50efc5d6
root <IBMBAROOEI>
2024-07-21 14:08:35 +0330

    create-resource-categorie

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -3,8 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Trait\ApiResponse;
+use Illuminate\Http\Response;
+
+use App\Interfaces\categoriRepositoryInterface;
 
 class CategorieController extends Controller
 {
-    //
+
+
+    Use ApiResponse;
+
+    public function __construct(private categoriRepositoryInterface $categoriRepositoryInterface )
+    {
+       $this->categoriRepositoryInterface= $categoriRepositoryInterface;
+    }
+
+
+
+     public function store(Request $request){
+        $product=$this->categoriRepositoryInterface->create($request->all());
+        return $this->handleStatusCodes(Response::HTTP_CREATED,  new ProductResource($product));
+     }
+
+
+
+
+
+     public function update(Request $request,$id){
+
+        $product=$this->productRepositoryInterface->update($id,$request->all());
+        return $this->handleStatusCodes(Response::HTTP_OK,  new ProductResource($product));
+     }
+
+
+
+      public function index(){
+
+
+        $products=$this->productRepositoryInterface->all();
+
+        return $this->handleStatusCodes(Response::HTTP_OK,
+        ProductResource::collection($products));
+      }
+
+
+      public function find($id){
+
+
+        $products=$this->productRepositoryInterface->find($id);
+
+        return $this->handleStatusCodes(Response::HTTP_OK,
+         new ProductResource($products));
+      }
+
+
+      public function delete(Product $product){
+
+
+        if($this->productRepositoryInterface->delete($product)){
+            return $this->handleStatusCodes(Response::HTTP_NO_CONTENT);
+        }
+        else{
+
+        return $this->handleStatusCodes(Response::HTTP_NOT_FOUND);
+
+      }
+    }
 }
