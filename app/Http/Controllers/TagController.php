7dfeac6a752e8d9f4b2606eb150974924d6b756b
root <IBMBAROOEI>
2024-07-23 12:57:27 +0330

    create-tag-crud

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Trait\ApiResponse;
+use App\Http\Resources\TagResource;
+
+use App\Interfaces\TagRepositoryInterface;
 
 class TagController extends Controller
 {
-    //
+
+
+    Use ApiResponse;
+
+    public function __construct(private TagRepositoryInterface $tagRepositoryInterface )
+    {
+       $this->tagRepositoryInterface= $tagRepositoryInterface;
+    }
+
+
+
+     public function store(Request $request){
+
+        $categori=$this->tagRepositoryInterface->create($request->all());
+        return $this->handleStatusCodes(Response::HTTP_CREATED,  new TagResource($categori));
+     }
+
+
+public function update(Tag $tag,Request $request)
+{
+    $c = $this->tagRepositoryInterface->update($tag,$request->all());
+    return $this->handleStatusCodes(Response::HTTP_OK, new TagResource($c));
+}
+
+      public function index(){
+
+
+        $categori=$this->tagRepositoryInterface->all();
+
+        return $this->handleStatusCodes(Response::HTTP_OK,
+        TagResource::collection($categori));
+      }
+
+
+
+
+      public function find(Tag $Tag)
+      {
+          if ($Tag) {
+              return $this->handleStatusCodes(Response::HTTP_OK, new TagResource($Tag));
+          } else {
+              return $this->handleStatusCodes(Response::HTTP_NOT_FOUND);
+          }
+      }
+
+
+
+      public function delete(Tag $tag)
+      {
+          $this->tagRepositoryInterface->delete($tag);
+          return $this->handleStatusCodes(Response::HTTP_NO_CONTENT);
+      }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,36 +4,39 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\TagController;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+   return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::prefix('product')->group(function(){
-Route::controller(ProductController::class)->group(function(){
- Route::post('/','store');
-   Route::get('/','index');
-   Route::get('/{product}','find');
-   Route::put('/{product}','update');
-   Route::delete('/{product}','delete');
-
+Route::prefix('product')->group(function () {
+   Route::controller(ProductController::class)->group(function () {
+      Route::post('/', 'store');
+      Route::get('/', 'index');
+      Route::get('/{product}', 'find');
+      Route::put('/{product}', 'update');
+      Route::delete('/{product}', 'delete');
    });
 });
 
-Route::prefix('categorie')->group(function(){
-    Route::controller(CategorieController::class)->group(function(){
-     Route::post('/','store');
-       Route::get('/','index');
-     Route::get('/{categorie}','find');
-      //  Route::get('/{id}','show');
+Route::prefix('categorie')->group(function () {
+   Route::controller(CategorieController::class)->group(function () {
+      Route::post('/', 'store');
+      Route::get('/', 'index');
+      Route::get('/{categorie}', 'find');
+      Route::put('/{categorie}', 'update');
+      Route::delete('/{categorie}', 'delete');
+   });
+});
 
-       Route::put('/{categorie}','update');
-       Route::delete('/{categorie}','delete');
-
-       });
-    });
-
-
-
-
+Route::prefix('tag')->group(function () {
+   Route::controller(TagController::class)->group(function () {
+      Route::post('/', 'store');
+      Route::get('/', 'index');
+      Route::get('/{tag}', 'find');
+      Route::put('/{tag}', 'update');
+      Route::delete('/{tag}', 'delete');
+   });
+});
